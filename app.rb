325a770927689772be4cd2f8b1381ce624f09bdb
Mansoor  Cheyo <mcheyo.mc@gmail.com>
2020-01-06 09:33:48 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do 
+        "My name is Cheyo"
+    end
+    
+    get '/hometown' do 
+        "My hometown is Dar"
+    end 
+
+    get '/favorite-song' do 
+        "My favorite song is Ha!"
+     end 
+
 end
